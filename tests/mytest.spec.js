ca3f99eb774ad29551e95358b4f2dc2b0993f7ef
truongnxse05891 <93703575+truongnxse05891@users.noreply.github.com>
2022-12-28 01:59:17 +0700

    update

--- tests/mytest.spec.js
+++ tests/mytest.spec.js
@@ -14,8 +14,8 @@ test('Redirect to Gmail page', async ({ page }) => {
 
 test('Redirect to Login page', async ({ page }) => {
   await page.goto('https://www.google.com/');
-  const gmailPage = page.locator("a.gb_7.gb_8.gb_de.gb_dd", {hasText: 'Đăng nhập' });
-  await gmailPage.click();
+  const signUP = page.locator("a.gb_7.gb_8.gb_de.gb_dd", {hasText: 'Đăng nhập' });
+  await signUP.click();
   
   await expect(page).toHaveTitle('Đăng nhập - Tài khoản Google');
 });
